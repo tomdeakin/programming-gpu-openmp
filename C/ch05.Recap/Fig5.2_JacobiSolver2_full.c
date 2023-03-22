e685873bfa1456b327f8f9c5b3852b6c5822792b
Tom Deakin <tom.deakin@bristol.ac.uk>
2023-03-22 16:07:58 +0000

    Add Chapter 5 code

---
+++ C/ch05.Recap/Fig5.2_JacobiSolver2_full.c
@@ -0,0 +1,108 @@
+/*
+
+NAME: Jacobi Solver with target data region
+
+Description: Solves the linear system Ax=b using the Jacobi method
+
+History: Written by Tom Deakin and Tim Mattson March 2023.
+
+License: Creative Commons with Attribution (CC BY).  See the file ../../License for details
+*/
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define TOLERANCE 0.001
+#define MAX_ITERS 100000
+
+void initializeProblem(int N, double *A, double *b, double *xold, double *xnew) {
+
+  // Create a random, diagonally dominant matrix
+  // After randomly initalizing the matrix, add the sum of 
+  // the row to the diagonal element on that row,
+  // then scale by that sum
+  for (int i = 0; i < N; i++) {
+    double sum = 0.0;
+    for (int j = 0; j < N; j++) {
+      A[i * N + j] = (rang() % 23) / 1000.0;
+      sum += A[i * N + j];
+    }
+    A[i * N + i] += sum;
+
+    // scale the row so the final matrix is almost an identity matrix
+    for (int j = 0; j < N; j++)
+      A[i * N + i] /= sum;
+  }
+}
+
+int main() {
+  int iters = 0,   N = 1000; // A[N][N]
+  double conv=100000.0, err, chksum, *A, *b, *xold, *xnew;
+  A = (double *)malloc(N * Ndim * sizeof(double));
+  b = (double *)malloc(N * sizeof(double));
+  xold = (double *)malloc(N * sizeof(double));
+  xnew = (double *)malloc(N * sizeof(double));
+  initializeProblem(N,A,b,xold,xnew);
+
+#pragma omp target data map(to: xold[0:N], A[0:N*N], b[0:N]) \
+                                         map(to:xnew[0:N])
+{
+  while ((conv > TOLERANCE) && (iters < MAX_ITERS)) {
+    iters++;
+    
+    #pragma omp target 
+    #pragma omp loop 
+    for (int i = 0; i < N; i++) {
+      xnew[i] = (double)0.0;
+      for (j = 0; j < N; j++) 
+        if (i != j)   xnew[i] += A[i * N + j] * xold[j];
+      xnew[i] = (b[i] - xnew[i]) / A[i * Ndim + i];
+    }
+    // test convergence
+    conv = 0.0;
+    
+  #pragma omp target map(tofrom: conv)
+  #pragma omp loop reduction(+ : conv)
+   for (int i = 0; i < N; i++) {
+     double tmp = xnew[i] - xold[i];
+     conv += tmp * tmp;
+    }
+    conv = sqrt(conv);
+
+    // pointer swap
+    double* tmp = xold;
+    xold = xnew;
+    xnew = tmp;
+  }
+  #pragma omp target update from(xnew[0:N])
+} // end of target data region
+
+  // Check solution by multiplying xnew by A and comparing to b
+  // reuse xold to store the result
+  double err = 0.0;
+  for (int i = 0; i < N; ++i) {
+    xold[i] = 0.0;
+    for (int j = 0; j < N; ++j)_ {
+      xold[i] += A[i * N + j] * xnew[j];
+    }
+    double tmp = xold[i] - b[i];
+    err += tmp * tmp;
+  }
+  err = sqrt(err);
+
+  printf("Jacobi Solver: err = %lf\n", err);
+
+  free(A);
+  free(b);
+  free(xold);
+  free(xnew);
+
+  if (err > TOLERANCE) {
+    printf("\nWARNING: final solution error > %g\n\n", TOLERANCE);
+    return EXIT_FAILURE;
+  }
+  else
+    return EXIT_SUCCESS;
+}
---
+++ C/ch05.Recap/Fig5.3_stencil_full.c
@@ -0,0 +1,121 @@
+/*
+* PROGRAM: heat equation solve
+*
+* PURPOSE: This program will explore use of an explicit
+*          finite difference method to solve the heat
+*          equation under a method of manufactured solution (MMS)
+*          scheme. The solution has been set to be a simple 
+*          function based on exponentials and trig functions.
+*
+*          A finite difference scheme is used on a 1000x1000 cube.
+*          A total of 0.5 units of time are simulated.
+*
+*          The MMS solution has been adapted from
+*          G.W. Recktenwald (2011). Finite difference approximations
+*          to the Heat Equation. Portland State University.
+*
+*
+* USAGE:   Run with two arguments:
+*          First is the number of cells.
+*          Second is the number of timesteps.
+*
+*          For example, with 100x100 cells and 10 steps:
+*
+*          ./heat 100 10
+*
+*
+* HISTORY: Written by Tom Deakin, Oct 2018
+*
+* LICENSE: Creative Commons with Attribution (CC BY).  See the file ../../License for details
+*
+*/
+
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+#define PI acos(-1.0) // Pi
+
+// Sets the mesh to an initial value, determined by a MMS scheme
+void initial_value(const int n, const double dx, const double length, double * restrict u) {
+  double y = dx;
+  for (int j = 0; j < n; ++j) {
+    double x = dx; // Physical x position
+    for (int i = 0; i < n; ++i) {
+      u[i+j*n] = sin(PI * x / length) * sin(PI * y / length);
+      x += dx;
+    }
+    y += dx; // Physical y position
+  }
+}
+
+// Set the n-by-n array u_tmp to zero
+void zero(int n, double *u_tmp) {
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      u_tmp[i+j*n] = 0.0;
+}
+
+int main(int argc, char *argv[]) {
+  int n = 1000;		// Problem size, forms an nxn grid
+  int nsteps = 10;	// Number of timesteps
+  double alpha = 0.1;	// heat equation coefficient
+  double length = 10000;// physical size of domain: length x length square
+  double dx = length/(n+1);// physical size of each cell  
+  double dt = 0.5 / nsteps;	// time interval (total time of 0.5s)
+  double r = alpha * dt / (dx * dx); // Stability
+  if (r > 0.5) print("WARNING: Unstable!\n");
+
+  // Allocate two nxn grids
+  double *u  = malloc(sizeof(double)*n*n);
+  double *u_tmp = malloc(sizeof(double)*n*n);
+  double *tmp;
+
+  initial_values(n, dx, length, u);
+  zero(n, u_tmp);
+  
+  const double r2 = 1.0 - 4.0*r;
+  for (int t = 0; t < nsteps; ++t) {
+  
+  #pragma omp target map(tofrom: u[0:n*n], utmp[0:n*n])
+  #pragma omp loop collapse(2)
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        utmp[i+j*n] =  r2 * u[i+j*n] +
+        r * ((i < n-1) ? u[i+1+j*n] : 0.0) +
+        r * ((i > 0)   ? u[i-1+j*n] : 0.0) +
+        r * ((j < n-1) ? u[i+(j+1)*n] : 0.0) +
+        r * ((j > 0)   ? u[i+(j-1)*n] : 0.0);
+      }
+    }
+    // Pointer swap
+    tmp = u;
+    u = utmp;
+    u_tmp = tmp;
+  }
+
+  // Check solution against the known solution defined by the MMS
+  double l2norm = 0.0;
+  double time = dt * double (nsteps); // total time
+  double y = dx;
+  for (int i = 0; i < n; ++i) {
+    double x = dx;
+    for (int j = 0; j < n; ++j) {
+      double correct = exp(-2.0*alpha*PI*PI*t/(length*length)) *
+        sin(PI*x/length) * sin(PI*y/length);
+      double tmp = u[i+j*n] - correct;
+      l2norm += tmp * tmp;
+      x += dx;
+    }
+    y += dy;
+  }
+  l2norm = sqrt(l2norm);
+
+  printf("Error l2norm: %lf\n", l2norm);
+
+  free(u);
+  free(u_tmp);
+
+  return EXIT_SUCCESS;
+}
+
