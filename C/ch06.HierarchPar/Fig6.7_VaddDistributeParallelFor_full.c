82ca99a3c5d010ef7cc2cf18102e50b707465801
Tom Deakin <tom.deakin@bristol.ac.uk>
2023-03-31 17:30:12 +0100

    Add Chapter 6 code

---
+++ C/ch06.HierarchPar/Fig6.7_VaddDistributeParallelFor_full.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <math.h>
+
+#define N 4000
+
+int main()
+{
+
+   float A[N], B[N], C[N];
+   // initialization
+   for (int i = 0; i < N; ++i) {
+     A[i] = 1.0f; B[i] = 2.0f; C[i] = 0.0f;
+   }
+
+   #pragma omp target
+   #pragma omp teams distribute parallel for
+   for (int i = 0; i < N; i++) {
+      C[i] = A[i] + B[i];
+   }
+
+   // Check solution
+   for (int i = 0; i < N; i++) {
+     assert(fabs(C[i] - 3.0f) < 1.0E-5);
+   }
+}
